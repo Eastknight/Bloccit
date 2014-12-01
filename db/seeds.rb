d511018f6ba5be7a7757ad8f03604d3a7cc28995
Lei Chen <leichenscu@gmail.com>
2014-11-30 19:23:34 -0500

    Added association between user and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,26 @@
 require 'faker'
 
 # Create Posts
+5.times do 
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 50.times do 
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
 end
 posts = Post.all
 
-#Create Comments
 100.times do
   Comment.create!(
     post: posts.sample,
@@ -17,6 +28,14 @@ posts = Post.all
   )
 end
 
-puts "Send finished"
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'leichenscu@gmail.com',
+  password: 'helloworld'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
