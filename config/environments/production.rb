ea89380a29e21534fcbc11bef974fb923f826932
Lei Chen <leichenscu@gmail.com>
2014-12-03 19:52:16 -0800

    Fixed a bug by changing the defalt mailer host in production ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {host: 'bloccit.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'leichen-bloccit.herokuapp.com'}
 end
