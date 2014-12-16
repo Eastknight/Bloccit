b2789e4fa41246e73e0bc132fd8bf29161c5e44e
Lei Chen <leichenscu@gmail.com>
2014-12-16 08:29:09 -0800

    Added the public profile page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.visible_to(current_user)
     @comments = @user.comments
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   default_scope { order('rank DESC') } 
+  scope :visible_to, -> (user) {user ? all : joins(:topic).where('topics.public' => true)}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
